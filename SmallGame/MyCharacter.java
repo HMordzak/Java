85595cd92f43025deeed4f87f548632f82453cb8
HMordzak <hubert.mordzak@gmail.com>
2016-05-24 16:10:19 +0200

    Create MyCharacter.java

---
+++ SmallGame/MyCharacter.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smallgame;
+
+/**
+ *
+ * @author Hubert
+ */
+public class MyCharacter extends Character {
+    
+    
+    
+    public MyCharacter(){
+        super();
+       
+        this.setAP(25);
+        this.setHP(1000);
+        this.setHit(90);
+        this.setCrit(20);
+    }
+    
+}
