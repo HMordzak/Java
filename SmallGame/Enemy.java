19663e6ce2547bc330ec3a374e7c90b7b54ac848
HMordzak <hubert.mordzak@gmail.com>
2016-05-24 16:08:47 +0200

    Create Enemy.java

---
+++ SmallGame/Enemy.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smallgame;
+
+/**
+ *
+ * @author Hubert
+ */
+public class Enemy extends Character {
+    
+   
+    
+    public Enemy(){
+        super();
+        
+        this.setHP(800);
+        this.setAP(20);
+        this.setHit(60);
+        this.setCrit(30);
+    }
+    
+    
+    
+}
